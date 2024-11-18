3362a4c8e14d449cfdcd5e7f46df3dd381aa00f9
haripriya7390 <159610618+haripriya7390@users.noreply.github.com>
2024-11-18 09:54:04 +0530

    Add files via upload

---
+++ second-react-app/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Srisailam Temple</h1>
+     <img src="https://tse3.mm.bing.net/th?id=OIP.gRSgMYPxHtVE2obeXg4PwAHaDR&pid=Api&P=0&h=180"></img>
+     <p>Sri Bhramarambika sametha Mallikarjuna swamy Devasthanam (TR: Mallikārjunuḍu) or Srisailam Temple is a Hindu temple dedicated to the deities Shiva and Parvati, located at Srisailam in the Indian state of Andhra Pradesh. It is significant to the Hindu sects of both Shaivism and Shaktism as this temple is referred to as one of the twelve Jyotirlingas of Shiva and as one of the eighteen Shakti pithas, centres of the Hindu goddess.[1] Shiva is worshiped as Mallikarjuna and is represented by the lingam. His consort Parvati is depicted as Bhramaramba.
+     When Shiva and Parvati decided to find suitable brides for their sons. Shiva got Buddhi (intellect) and Siddhi (spiritual power) married to Ganesha. Kartikeya on his return was enraged and went away to stay alone on Mount Krauncha in Palani in the name of Kumara brahmachari. On seeing his father coming over to pacify him, he tried to move to another place, but on the request of the Devas, stayed close by. The place where Shiva and Parvati stayed came to be known as Srisailam.[2]
+
+As per Hindu legend, the presiding deity in the form of Linga (an iconic form of Shiva) was worshipped with jasmine (locally called in Telugu as Mallika), leading to the name of presiding deity as Mallikarjuna.[3]
+     </p>
+     <hr></hr>
+     <h1>Durga Temple</h1>
+     <img src="https://tse2.mm.bing.net/th?id=OIP.YIqlTFPJKN28wLcfh2kKJQHaE8&pid=Api&P=0&h=180"></img>
+     <p>Kanaka Durga Temple, officially known as Sri Durga Malleswara Swamyvarla Devasthanam, is a Hindu temple dedicated to Kanaka Durga. The deity in this temple is also popularly referred as Kanaka Durga. The temple is located in Vijayawada, Andhra Pradesh, India on the Indrakeeladri hill on the banks of Krishna River.[1] Kaalika Purana, Durgaa Sapthashati and other Vedic literature have mentioned about Kanaka Durga on the Indrakeelaadri and have described the deity as Swayambhu, (self-manifested) in Triteeya Kalpa</p>
+     <hr></hr>
+     <h1>Kanipakam Temple</h1>
+     <img src="https://tse2.mm.bing.net/th?id=OIP.osyOLufs7GtyrLKgSXXqSAHaEH&pid=Api&P=0&h=180"></img>
+     <p>According to legend, there were three brothers who were mute, deaf and blind. They were digging a well to fetch water to their field. The device they were using fell into the well hitting hard object. When they dug further, blood started to gush out of the well and the three got rid of their disabilities. The villagers rushed to the spot and found deity of Ganesha. Villagers dug further, but they were not able to find the base of the deity. The deity sits in the well which is always full of water.[2]
+
+The idol has apparently gotten larger over time. This is evidenced by the fact that a silver armour of the deity from 50 year ago, doesn't fit today. Currently only the knee and abdomen of the deity is visible.[2]</p>
+     <hr></hr>
+     <h1>Srikalahasti Temple</h1>
+     <img src="https://tse2.mm.bing.net/th?id=OIP.xcqm3kzvmiwi-LthJY-7nwHaFr&pid=Api&P=0&h=180"></img>
+     <p>The Srikalahasti Temple is located in the town of Srikalahasti, in Tirupati district in the state of Andhra Pradesh, India. According to regional tradition, it is said to be the site where Kannappa was ready to offer both his eyes to cover blood flowing from the Shivalinga before Shiva stopped him and granted him moksha.[3] The inner temple was constructed around the 5th century CE and the outer temple was constructed in the 11th century by the Rajendra Chola I and other Chola emperors such as Rajaditya Chola, Rajaraja Chola I, Rajadhiraja Chola I, Kulottunga Chola I, Kulottunga Chola III, and Reddi kingdom,Vijayanagar Empire. The 120 feet (37 m) high main gopuram and the hundred pillared hall with intricate carvings was commissioned during the regime of the Vijayanagara Krishnadevaraya during 1516 CE.[4] Shiva in his aspect as Vayu is worshipped as Kalahasteeswara. The temple is also regarded as Rahu-Ketu kshetra and Dakshina Kailasam.
+
+Srikalahasti temple, situated 36 km away from Tirupati, is famous for its Vayu Lingam (Wind Lingam), one of the Pancha Bhuta Sthalams, representing the wind.
+
+Legend
+In primordial times, the wind-god Vayu performed penance for thousands of years to the Karpoora Lingam, the lingam of Shiva made of camphor. Pleased with Vayu's penance, Shiva manifested before him and bestowed him three boons. Vayu was blessed to present everywhere in the world in form of air and want to be an integral part of every being in form of vayus. Further, The linga that Vayu worshipped was named as the Vayu Lingam after him and was declared to be worshipped by various beings.[5]
+
+Another legend narrates that Shiva's consort Parvati adamantly asked him to teach her the secret of the Shiva Panchakshari Mantra. Angry with her behaviour, Shiva cursed her to discard her divine form and assume a human form. Parvati fell to the Earth. To atone, Parvati performed penance at Srikalahasti and pleased Shiva. Shiva granted her a heavenly body, a hundred times better than her previous divine form, apologised to her and later taught her the secret lore. Parvati is therefore worshipped as Gnana Prasunambika Devi in the temple.
+
+Cursed to become a ghost, Ghanakala prayed at Srikalahasti for 15 years and after chanting the Bhairava Mantra, Shiva restored her original form.</p>
+     <hr></hr>
+    </div>
+  );
+}
+
+export default App;
